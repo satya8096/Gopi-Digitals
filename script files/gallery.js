361a98b023720ed33357d2484de1a50ff503e9a8
satya8096 <21MH5A0326@acoe.edu.in>
2024-06-19 21:17:07 +0530

    first commit

---
+++ script files/gallery.js
@@ -0,0 +1,11 @@
+// document.addEventListener("click",function(e){
+//     if(e.target.classList.contains("gallery-img")){
+//         const src = e.target.getAttribute("src");
+//         document.querySelector(".img-show-container img").src = src;
+//         document.querySelector(".img-show-container").style.scale = "1";
+//     }
+// })
+
+// document.querySelector(".btn-close").onclick = () =>{
+//     document.querySelector(".img-show-container").style.scale = "0"
+// }
